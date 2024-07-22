ac5af10b32220ca2b05146b08dbb344ef4e7de7c
Braedon Lewis <braedonwilcox@gmail.com>
2024-07-22 00:10:04 -0400

    remove random image hotlinkng

--- themes/pelly/static/js/randomimage.js
+++ themes/pelly/static/js/randomimage.js
@@ -1,12 +1,49 @@
 window.onload = choosePic;
 
-var myPix = new Array("assets/pfpcrop.png","assets/otherpelly.jpg","https://i.redd.it/hsrm38mlf2o41.jpg","https://i.pinimg.com/originals/d7/57/22/d7572276e80bd31449710f6a28c3b6b1.jpg","https://emoji.gg/assets/emoji/JackBlackStop.png"
-,"https://i1.sndcdn.com/artworks-000205278199-77zuj6-t500x500.jpg","https://pbs.twimg.com/profile_images/997268877636550656/x0qmi5nO_400x400.jpg","https://steamuserimages-a.akamaihd.net/ugc/449582555676740346/108CA9A47F1B19C7655DA67935ED03944DFBD41F/"
-,"https://i.ytimg.com/vi/sDzmXF2SQf4/maxresdefault.jpg", "https://i.imgur.com/OBg0cHi.gif","https://i.makeagif.com/media/4-11-2017/Apquga.gif","https://wiki.nerd.nu/images/thumb/c/ca/OoOOooer.gif/500px-OoOOooer.gif"
-,"https://preview.redd.it/9hlskc5c6nc41.gif?width=640&crop=smart&s=8211a5a62294612779e8b7fef222fb22b3e69dc1","https://media.tenor.com/images/7219604c50d8c562f2848bf0ffc626f6/tenor.gif","https://i.ytimg.com/vi/i6wUBD5vBeo/hqdefault.jpg"
-,"https://media.suthlbr.com/products/images/30001/2967891_ep_1514387985_2.jpg","https://i.ytimg.com/vi/qbcnIenjKWg/hqdefault.jpg","https://static0.thegamerimages.com/wordpress/wp-content/uploads/2020/05/Xbox-Series-X-Fridge-Meme.jpg?q=50&fit=crop&w=740&h=389"); /* i'll add more eventually  */
+var myPix = new Array(
+	"assets/pfpcrop.png",
+	"assets/randomimages/0.jpg",
+	"assets/otherpelly.jpg",
+	"assets/randomimages/legacy/9hlskc5c6nc41.gif",
+	"assets/randomimages/legacy/500px-OoOOooer.gif",
+	"assets/randomimages/legacy/2967891_ep_1514387985_2.jpg",
+	"assets/randomimages/legacy/1437033731_OOOOOOOOAAAAAAAAAAAAH.gif",
+	"assets/randomimages/legacy/1Apquga.gif",
+	"assets/randomimages/legacy/artworks-000205278199-77zuj6-t500x500.jpg",
+	"assets/randomimages/legacy/d7572276e80bd31449710f6a28c3b6b1.jpg",
+	"assets/randomimages/legacy/hqdefault.jpg",
+	"assets/randomimages/legacy/hqdefault2.jpg",
+	"assets/randomimages/legacy/hsrm38mlf2o41.jpg",
+	"assets/randomimages/legacy/JackBlackStop.png",
+	"assets/randomimages/legacy/maxresdefault.jpg",
+	"assets/randomimages/legacy/OBg0cHi.gif",
+	"assets/randomimages/legacy/tenor.gif",
+	"assets/randomimages/legacy/x0qmi5nO_400x400.jpg",
+	); /* i'll add more eventually  */
+
+function downloadImage(url, name) {
+    fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+            let a = document.createElement('a');
+            let url = window.URL.createObjectURL(blob);
+            a.href = url;
+            a.download = name;
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(() => console.error('Could not download image: ' + url));
+}
 
 function choosePic() {
+	/*myPix.forEach((url, index) => {
+		let name = 'image' + (index + 1) + url.substring(url.lastIndexOf('.'));
+		downloadImage(url, name);
+		console.log("downloaded img");
+	});*/
+
 	randomNum = Math.floor((Math.random() * myPix.length));
 	document.getElementById("myPicture").src = myPix[randomNum];
 }
